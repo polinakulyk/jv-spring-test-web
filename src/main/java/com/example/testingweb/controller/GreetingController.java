ebf214c8b099251455184b33590513f143796703
Polina Kulyk <polina.kulyk@protonmail.com>
2020-08-06 13:22:46 +0300

    Tested an app with JUnit and Mockito, while isolation web layer

---
+++ src/main/java/com/example/testingweb/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.testingweb.controller;
+
+import com.example.testingweb.service.GreetingService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody
+    String greeting() {
+        return greetingService.greet();
+    }
+}
